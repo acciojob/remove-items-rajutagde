33d5359c5d993692b4d9af7832a45483085aeecb
Rajkirantagde <121598976+rajutagde@users.noreply.github.com>
2023-04-13 22:12:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let removeBtn = document.querySelector('input[type="button"]');
+let colorSelect = document.querySelector('#colorSelect');
+
+removeBtn.addEventListener('click', function() {
+  let selectedColor = colorSelect.value;
+  let colorOption = colorSelect.querySelector(`[value="${selectedColor}"]`);
+  
+  if (colorOption) {
+    colorSelect.removeChild(colorOption);
+  }
+});
